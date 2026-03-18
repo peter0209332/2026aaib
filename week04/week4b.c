e1615f30fba61dcd83f6da675faa61f1e98f1da2
peter0209332 <14750286@me.mcu.edu.tw>
2026-03-18 12:03:57 +0800

    Add files via upload

---
+++ week04/week4b.c
@@ -0,0 +1,11 @@
+//week04-5a.cpp SOIT107_BASE_004
+#include <stdio.h>
+int main()
+{S
+    scanf("%d", &N);
+    if (N>=90) printf("A");
+    else if (N>=80) printf("B");
+    else if (N>=70) printf("C");
+    else if (N>=60) printf("D");
+    else printf("F");
+}
